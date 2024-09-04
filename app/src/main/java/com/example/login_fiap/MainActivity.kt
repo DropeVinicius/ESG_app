e68f3e2998ed1314f207d985d61ac4a4932aa404
Ricardo Pinheiro <ricardspin@gmail.com>
2024-09-03 21:57:17 -0300

    Tela home + Navegação

--- app/src/main/java/com/example/login_fiap/MainActivity.kt
+++ app/src/main/java/com/example/login_fiap/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.example.login_fiap
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.login_fiap.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.btnLogin.setOnClickListener{
+            val telaHome = Intent(this,Home::class.java)
+            startActivity(telaHome)
+        }
 
     }
 }
